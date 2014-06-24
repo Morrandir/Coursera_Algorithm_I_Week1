fe7de42a680a4a52293cdc0bc0dc7958c941385e
Qubo Song <sqb_007@hotmail.com>
2014-06-24 13:50:11 +0800

    Problem done...

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -3,68 +3,76 @@
  */
 
 public class PercolationStats {
-    private int size;
     private int count;
-    private double pThreshold[];
+    private double[] pThreshold;
     private double mean = -1;
     private double stddev = -1;
     private double confLo;
     private double confHi;
 
-    public PercolationStats(int N, int T) {    // perform T independent computational experiments on an N-by-N grid
-        if(N <= 0 || T <= 0) {
-            throw new java.lang.IllegalArgumentException();
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
         }
         int i, j;
-        size = N;
         count = T;
 
         pThreshold = new double[count];
 
         for (int t = 0; t < count; t++) {
-            Percolation p = new Percolation(size);
+            Percolation p = new Percolation(N);
             pThreshold[t] = 0;
 
-            for (int n = 0; n < size * size; n++) {
+            for (int n = 0; n < N * N; n++) {
                 do {
-                    i = StdRandom.uniform(size);
-                    j = StdRandom.uniform(size);
+                    i = StdRandom.uniform(N);
+                    j = StdRandom.uniform(N);
                 } while(p.isOpen(i, j));
                 p.open(i, j);
                 pThreshold[t] += 1;
-                if(p.percolates()) {
+                if (p.percolates()) {
                     break;
                 }
             }
-            pThreshold[t] /= size * size;
+            pThreshold[t] /= N * N;
         }
     }
-    public double mean() {                     // sample mean of percolation threshold
+
+    // sample mean of percolation threshold
+    public double mean() {
         double sum = 0;
-        for(int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             sum += pThreshold[i];
         }
         return sum / count;
     }
 
-    public double stddev() {                   // sample standard deviation of percolation threshold
+    // sample standard deviation of percolation threshold
+    public double stddev() {
         double deviation = 0;
-        for(int i = 0; i < count; i++) {
-            deviation += Math.pow((pThreshold[i] - mean), 2);
+        for (int i = 0; i < count; i++) {
+            deviation += (pThreshold[i] - mean) * (pThreshold[i] - mean);
         }
         return Math.sqrt(deviation / (count - 1));
     }
 
-    public double confidenceLo() {             // returns lower bound of the 95% confidence interval
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo() {
         return (mean - 1.96 * stddev / Math.sqrt(count));
     }
 
-    public double confidenceHi() {             // returns upper bound of the 95% confidence interval
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi() {
         return (mean + 1.96 * stddev / Math.sqrt(count));
     }
 
-    public static void main(String[] args) {   // test client, described below
-        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    // test client, described below
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        Stopwatch sw = new Stopwatch();
+        PercolationStats ps = new PercolationStats(N, T);
         ps.mean = ps.mean();
         ps.stddev = ps.stddev();
         ps.confLo = ps.confidenceLo();
@@ -80,5 +88,8 @@ public class PercolationStats {
         System.out.print(", ");
         System.out.print(ps.confHi);
         System.out.print("\n");
+
+        System.out.print("elapsed time              = ");
+        System.out.print(sw.elapsedTime());
     }
 }
